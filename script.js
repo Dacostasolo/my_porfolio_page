f991e4c575e30532132a864d3b0e20b6e37eb3da
Dacosta Solomon <dacsolo10@gmail.com>
2022-06-08 20:28:52 +0000

    added sticky navigatio and lazy image load

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 
 const nav = document.querySelector('.nav')
 const header = document.querySelector('.hero')
+const aboutImage = document.querySelectorAll('.about-item__image')
+const overlay = document.querySelectorAll('.overlay')
+
+//implementing sticky navigation bar
 const callBack = function (entries) {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -21,5 +25,26 @@ const options = {
 }
 
 const observer = new IntersectionObserver(callBack, options)
-
 observer.observe(header)
+
+let counter = 0
+const aboutCallBack = function (entries,observer) {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      overlay[counter++].classList.add('about-item--visible')
+      observer.unobserve(entry)
+    }
+  })
+}
+
+const aboutOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5,
+}
+
+const aboutImageObserver = new IntersectionObserver(aboutCallBack, aboutOptions)
+
+aboutImage.forEach((image) => {
+  aboutImageObserver.observe(image)
+})
